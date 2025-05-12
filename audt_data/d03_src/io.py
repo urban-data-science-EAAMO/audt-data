6d0ab0faf8c5a294afb556ad2c35bf84dcff1e84
Matt Franchi <mattwfranc@gmail.com>
2025-05-12 16:51:15 -0400

    new york pois extract reference

--- audt_data/d03_src/io.py
+++ audt_data/d03_src/io.py
@@ -12,3 +12,4 @@ INSTALL_DIR="/share/garg/uds_audt/matt/audt-data"
 #Data files:
 #
 ZBP_CSV='/share/garg/uds_audt/static/ZBP/all_NYC.csv'
+OSM_POIS_NY_CURRENT='/share/garg/uds_audt/static/new_york_pois_current.csv'
