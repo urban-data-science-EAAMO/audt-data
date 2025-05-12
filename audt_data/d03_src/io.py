ed0ffffe43859ea30143b5142124d6be1c0c2af4
Gabriel Spessato Agostini <gs665@lisbeth.tech.cornell.edu>
2025-05-12 16:41:02 -0400

    ZBP data

--- audt_data/d03_src/io.py
+++ audt_data/d03_src/io.py
@@ -7,3 +7,8 @@
 """
 
 INSTALL_DIR="/share/garg/uds_audt/matt/audt-data"
+
+#######################################################################
+#Data files:
+#
+ZBP_CSV='/share/garg/uds_audt/static/ZBP/all_NYC.csv'
